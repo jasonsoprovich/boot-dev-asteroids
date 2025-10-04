2671a6fc94e9ebf68da7ccdc7eedb3e4c2883c37
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-10-03 18:15:30 -0600

    feat: add fps clock

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    game_loop(screen)
-    pygame.quit()
+    clock = pygame.time.Clock()
+    dt = 0
 
-def game_loop(screen):
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0, 0, 0))
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
