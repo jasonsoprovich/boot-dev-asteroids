09fba46a20f7c0a123ed8179f300c67662804ec0
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-10-03 19:31:01 -0600

    feat: add updatable and drawable groups

--- main.py
+++ main.py
@@ -6,6 +6,11 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -14,9 +19,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
+        updatable.update(dt)
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
