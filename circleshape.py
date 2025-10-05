c680059f68291cbe369d60d25b138e19f40ab2da
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-10-04 22:23:39 -0600

    feat: add object collision

--- circleshape.py
+++ circleshape.py
@@ -11,8 +11,11 @@ class CircleShape(pygame.sprite.Sprite):
     self.velocity = pygame.Vector2(0, 0)
     self.radius = radius
 
-    def draw(self, screen):
-      pass
+  def draw(self, screen):
+    pass
 
-    def update(self, dt):
-      pass
+  def update(self, dt):
+    pass
+
+  def check_collision(self, object):
+    return self.position.distance_to(object.position) <= self.radius + object.radius
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
-    
+
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
@@ -27,6 +27,12 @@ def main():
                 return
             
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.check_collision(player):
+                print("Game over!")
+                sys.exit()
+
         screen.fill("black")
 
         for obj in drawable:
