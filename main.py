f0e70ce0d103bfc4711259a4ce294d700b021f6d
jasonsoprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-10-05 20:18:05 -0600

    feat: add basic shooting functionality

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import sys
 import pygame
 from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
     pygame.init()
@@ -12,10 +14,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
-    AsteroidField.containers = (updatable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
@@ -31,6 +35,7 @@ def main():
         for asteroid in asteroids:
             if asteroid.check_collision(player):
                 print("Game over!")
+                pygame.quit()
                 sys.exit()
 
         screen.fill("black")
